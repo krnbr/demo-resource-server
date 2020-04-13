151f8578c17465a60ede750d7266e36c3fe8198f
Karanbir Singh <krnbr@live.in>
2020-04-13 09:42:46 +0530

    init

---
+++ src/test/kotlin/in/neuw/resource/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package `in`.neuw.resource.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
