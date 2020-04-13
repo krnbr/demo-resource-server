c5a772ee5c65d80d562385b0cbc176b87a504d1f
Karanbir Singh <krnbr@live.in>
2020-04-13 09:50:02 +0530

    ignore/disable auto security

--- src/main/kotlin/in/neuw/resource/demo/DemoApplication.kt
+++ src/main/kotlin/in/neuw/resource/demo/DemoApplication.kt
@@ -1,9 +1,11 @@
 package `in`.neuw.resource.demo
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
 class DemoApplication
 
 fun main(args: Array<String>) {
