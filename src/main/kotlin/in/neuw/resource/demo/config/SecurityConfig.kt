e6903ecc1d01eb8a1a3144bfe1b4b0f08994c92e
Karanbir Singh <krnbr@live.in>
2020-04-14 12:08:00 +0530

    custom SecurityConfig for doing customizations

--- src/main/kotlin/in/neuw/resource/demo/config/SecurityConfig.kt
+++ src/main/kotlin/in/neuw/resource/demo/config/SecurityConfig.kt
@@ -1,7 +1,40 @@
 package `in`.neuw.resource.demo.config
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest
+import org.springframework.boot.actuate.health.HealthEndpoint
+import org.springframework.boot.actuate.info.InfoEndpoint
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import java.util.*
 
 @EnableReactiveMethodSecurity
-class SecurityConfig {
+class SecurityConfig: WebFluxConfigurer {
+
+    @Autowired
+    private lateinit var resourceServerProperties: OAuth2ResourceServerProperties
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtDecoder(decoder())
+
+        return http.build()
+    }
+
+    private fun decoder(): ReactiveJwtDecoder? {
+        return NimbusReactiveJwtDecoder(Objects.requireNonNull(resourceServerProperties!!.jwt.jwkSetUri))
+    }
+
 }
