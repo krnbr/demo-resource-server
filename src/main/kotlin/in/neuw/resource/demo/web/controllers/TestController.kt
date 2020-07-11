bf20b2081126295fd59419b1df97c6b31081f4b2
Karanbir Singh <krnbr@live.in>
2020-07-11 20:32:33 +0530

    changing the ports for the different setup & added logs

--- src/main/kotlin/in/neuw/resource/demo/web/controllers/TestController.kt
+++ src/main/kotlin/in/neuw/resource/demo/web/controllers/TestController.kt
@@ -1,6 +1,8 @@
 package `in`.neuw.resource.demo.web.controllers
 
 import `in`.neuw.resource.demo.models.TestDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +14,13 @@ import reactor.core.publisher.Mono
 @RequestMapping("/api/v1/")
 class TestController {
 
+    private val log:Logger = LoggerFactory.getLogger(TestController::class.java)
+
     @PreAuthorize("hasAuthority('SCOPE_system')")
     @GetMapping("test")
     fun getTest(@RequestParam(defaultValue = "test", required = false) message:String): Mono<TestDto> {
+        log.info("/api/v1/test endpoint hit")
+        log.info("query param message value is : {}", message)
         return Mono.just(TestDto(message));
     }
 
