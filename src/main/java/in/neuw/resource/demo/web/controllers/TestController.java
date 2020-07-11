0e53e8701dd197ab1780f673313aa3fb5a7b29f0
Karanbir Singh <krnbr@live.in>
2020-07-11 20:29:02 +0530

    changing the ports for the different setup & added logs

--- src/main/java/in/neuw/resource/demo/web/controllers/TestController.java
+++ src/main/java/in/neuw/resource/demo/web/controllers/TestController.java
@@ -1,6 +1,7 @@
 package in.neuw.resource.demo.web.controllers;
 
 import in.neuw.resource.demo.models.TestDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 /**
  * @author Karanbir Singh on 04/26/2020
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/")
 public class TestController {
@@ -18,6 +20,8 @@ public class TestController {
     @PreAuthorize("hasAuthority('SCOPE_system')")
     @GetMapping("test")
     public Mono<TestDto> getTest(@RequestParam(defaultValue = "test", required = false) String message) {
+        log.info("/api/v1/test endpoint hit");
+        log.info("query param message value is : {}", message);
         return Mono.just(new TestDto().setMessage(message));
     }
 
